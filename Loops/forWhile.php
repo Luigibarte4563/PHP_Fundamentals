410e289dbf607bec052b31515e92b31b7b102963
Luigi Barte <luigibarte1@gmail.com>
2026-01-01 21:22:27 +0800

    Add files via upload

---
+++ Loops/forWhile.php
@@ -0,0 +1,13 @@
+<?php
+// For Loop
+for($i = 1; $i <= 5; $i++) {
+    echo "For Loop: {$i} <br>";
+}
+
+// While Loop
+$j = 1;
+while ($j <= 5) {
+    echo "While Loop: {$j} <br>";
+    $j++;
+}
+?>

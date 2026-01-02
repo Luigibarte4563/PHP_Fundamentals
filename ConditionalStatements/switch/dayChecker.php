149b995e12c6ca70a102ddf6d578d024e81cbbc2
Luigi Barte <luigibarte1@gmail.com>
2026-01-02 21:07:20 +0800

    Add files via upload

---
+++ ConditionalStatements/switch/dayChecker.php
@@ -0,0 +1,15 @@
+<?php
+$day = "Monday";
+
+switch ($day) {
+    case "Monday":
+        echo "Start of the week";
+        break;
+    case "Friday";
+        echo "Almost weekend";
+        break;
+    default:
+        echo "Regular day";
+        break;
+}
+?>

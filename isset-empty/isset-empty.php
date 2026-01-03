41b3773a10e2a7723de9548bcdf47a007e2c60ba
Luigi Barte <luigibarte1@gmail.com>
2026-01-03 21:11:12 +0800

    Update isset-empty.php

--- isset-empty/isset-empty.php
+++ isset-empty/isset-empty.php
@@ -1,10 +1,12 @@
 <?php
 $name = "";
 
+//isset is the variable is exists and the value is NOT NULL
 if (isset($name)) {
     echo "Variable is set<br>";
 }
 
+//empty just checking if the variable have a value
 if (empty($name)) {
     echo "Variable is empty";
 }

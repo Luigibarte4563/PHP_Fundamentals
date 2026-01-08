1061c6e5c702f7fc96f0239af6a4a515e7808d07
Luigibarte4563 <luigibarte1@gmail.com>
2026-01-08 19:56:43 +0800

    Number to Roman Numerals

---
+++ ConditionalStatements/switch/numberToRoman.php
@@ -0,0 +1,24 @@
+<?php
+$number = 4;
+
+switch ($number) {
+    case 1:
+        echo "I";
+        break;
+    case 2;
+        echo "II";
+        break;
+    case 3;
+        echo "III";
+        break;
+    case 4;
+        echo "IV";
+        break;
+    case 5;
+        echo "V";
+        break;
+    default:
+        echo "Wrong input";
+        break;
+}
+?>

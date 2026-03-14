637d9041a3c4005ceedf6f6e84e1f999bc5a9a74
Luigibarte4563 <luigibarte1@gmail.com>
2026-03-14 21:37:13 +0800

    printing date

---
+++ date_and_time/dateTime.php
@@ -0,0 +1,7 @@
+<?php
+echo "Today is " . date("Y/m/d") . "<br>";
+echo "Today is " . date("Y.m.d") . "<br>";
+echo "Today is " . date("Y-m-d") . "<br>";
+echo "Today is " . date("l") . "<br>";
+echo date('l, F j, Y');
+?>

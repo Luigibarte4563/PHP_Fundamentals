80d754029602a6661a558f8425fdcf250243968d
Luigibarte4563 <luigibarte1@gmail.com>
2026-03-26 21:46:19 +0800

    filters

---
+++ filters/sanitizeAndValidate.php
@@ -0,0 +1,11 @@
+<?php
+$email = "john.doe@example.com";
+
+$email = filter_var($email, FILTER_SANITIZE_EMAIL);
+
+if (!filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
+    echo ("$email is a valid email address");
+} else {
+    echo ("$email is not valid email address");
+}
+?>

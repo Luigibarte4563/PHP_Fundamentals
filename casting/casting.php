6b608b211b35d2bc711a0218e62c71a093e89058
Luigibarte4563 <luigibarte1@gmail.com>
2026-03-27 22:18:33 +0800

    casting

---
+++ casting/casting.php
@@ -0,0 +1,14 @@
+<?php
+$a = 5;       // Integer
+$b = 5.34;    // Float
+$c = "hello"; // String
+$d = true;    // Boolean
+$e = NULL;    // NULL
+
+$a = (string) $a;
+$b = (string) $b;
+$c = (string) $c;
+$d = (string) $d;
+$e = (string) $e;
+
+?>
